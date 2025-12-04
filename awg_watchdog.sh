5f23d4d46f94662e8da2941dc9d9a8b2bcaab9f6
lm-james-ethan <limingjamesethan@gmail.com>
2025-12-04 23:28:52 +0630

    update watchdog

--- awg_watchdog.sh
+++ awg_watchdog.sh
@@ -1,64 +1,29 @@
 #!/bin/sh
-#
-# awg_watchdog.sh  –  Re-connect awg0 automatically
-#
-# • Checks IPv4 reachability through awg0
-# • On failure it:
-#     1) ip link set down awg0
-#     2) flushes addresses
-#     3) runs the AmneziaWG helper binaries to re-load awg0.conf
-#     4) re-adds the client address
-#     5) brings the interface back up
-# • Logs to syslog with tag “awg_watchdog”
-# • Intended for a cron entry:  * * * * * /root/awg_watchdog.sh
-#
-##############################################################################
-AWG_DIR="/data/usr/app/awg"
-CFG="$AWG_DIR/amnezia_for_awg.conf"
-IFCFG="$AWG_DIR/awg0.conf"
-AWG_BIN="$AWG_DIR/awg"
-AWG_GO="$AWG_DIR/amneziawg-go"
 
-# Targets to test THROUGH the tunnel
-TARGETS="1.1.1.1 8.8.8.8 9.9.9.9"
-PING_WAIT=2        # seconds
-TRIES=3            # how many different IPs must fail
+# Configuration
+CHECK_IP="1.1.1.1"
+# Points to the main switch script
+SWITCH_SCRIPT="/data/usr/app/awg/awg_route_switch.sh"
 
-##############################################################################
-# Parse static values from original config
-##############################################################################
-WG_ADDR=$(awk -F' = ' '/^Address/ {print $2}' "$CFG")
+# 1. Check if we intend to be online
+# If the state file doesn't exist, the user probably ran 'down', so we shouldn't intervene.
+if [ ! -f "/tmp/awg_wan_info" ]; then
+    exit 0
+fi
 
-##############################################################################
-# Ping-check awg0
-##############################################################################
-fail=0
-for ip in $TARGETS; do
-    if ping -I awg0 -c 1 -W $PING_WAIT "$ip" >/dev/null 2>&1; then
-        logger -t awg_watchdog "awg0 OK (ping $ip)"
-        exit 0
-    else
-        fail=$((fail+1))
-    fi
-    [ $fail -ge $TRIES ] && break
-done
+# 2. Check Interface existence
+if ! ip link show awg0 > /dev/null 2>&1; then
+    logger -t awg_watchdog "Interface awg0 missing. Restarting..."
+    sh "$SWITCH_SCRIPT" down
+    sh "$SWITCH_SCRIPT" up
+    exit 0
+fi
 
-##############################################################################
-# If here, all pings failed ? restart awg0
-##############################################################################
-logger -t awg_watchdog "awg0 FAIL ($fail/${#TARGETS}) – restarting..."
-
-# 1. Bring interface down and flush
-ip link set awg0 down 2>/dev/null
-ip addr flush dev awg0
-
-# 2. Re-load config
-"$AWG_GO" awg0 >/dev/null 2>&1
-"$AWG_BIN" setconf awg0 "$IFCFG"
-
-# 3. Re-add client address and bring link up
-ip addr add "$WG_ADDR" dev awg0
-ip link set up awg0
-
-logger -t awg_watchdog "awg0 restarted"
-exit 0
+# 3. Check Connectivity (Ping)
+# -c 1: One packet
+# -W 5: Wait 5 seconds
+if ! ping -c 1 -W 5 -I awg0 "$CHECK_IP" > /dev/null 2>&1; then
+    logger -t awg_watchdog "Ping check to $CHECK_IP failed. Restarting VPN..."
+    sh "$SWITCH_SCRIPT" down
+    sh "$SWITCH_SCRIPT" up
+fi
